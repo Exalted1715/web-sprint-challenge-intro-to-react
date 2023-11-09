9053679a6e888578a65de47ff1745ccd5a74eeef
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-11-09 09:20:58 -0700

    additional changes

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,9 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './Character';
 
-
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = 'http://localhost:9009/api/planets';
+const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
   const [planets, setPlanets] = useState([]);
@@ -19,34 +18,15 @@ function App() {
       setPeople(response.data);
     });
   }, []);
-
-  const buildPeopleCard = (obj) => {
-    return (
-      <div className="character-card" key={obj.id}>
-        <h3 className="character-name">{obj.name}</h3>
-        
-        <Character combinedData={obj} />
-      </div>
-    );
-  };
-
-  console.log(planets);
-  console.log(people);
-
-//combining the data to be able to grab the homeworld names
-const combinedData = people.map((character) => {
-  const homeworld = planets.find((planet) => planet.id === character.homeworld.id);
-  return {
-    ...character,
-    homeworld: homeworld ? homeworld.name : "Unknown",
-  };
-});
-
+console.log(people)
+console.log(planets)
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {people.map((person) => buildPeopleCard(person))}
+      {people.map((person) => (
+        <Character key={person.id} character={person} planets={planets} />
+      ))}
     </div>
   );
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 
-function Character({ combinedData }) {
+const Character = ({ character, planets }) => {
   const [showHomeworld, setShowHomeworld] = useState(false);
-
+  const [showPlanet, setShowPlanet] = useState(false)
   const handleClick = () => {
     setShowHomeworld((prevState) => !prevState);
+    setShowPlanet((prevState) => !prevState);
   };
 
-  return (  
-      <p onClick={handleClick} style={{ cursor: 'pointer' }}>Planet: {showHomeworld && (
-        <span className='character-planet'>{combinedData.homeworld}</span>
-      )}</p>
+  const homeworld = planets.find((planet) => planet.id === character.homeworld);
+  let homeworldName = 'Unknown'
+  if (homeworld) {
+  homeworldName = homeworld.name
+  }
+
+  return (
+    <div onClick={handleClick} style={{ cursor: 'pointer' }} className="character-card">
+      <h3 className="character-name">{character.name}</h3>
+      {showPlanet && <p>Planet: {showHomeworld && <span className='character-planet'>{homeworldName}</span>} </p>}
       
-    
+    </div>
   );
-}
+};
 
 export default Character;
